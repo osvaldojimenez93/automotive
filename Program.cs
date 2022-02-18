2e2330de3a86efc433a9bec2c084b0aac9f347d9
osvaldojimenez93 <osvaldo.jimenez93@outlook.com>
2022-02-18 11:27:16 -0600

    cleaned up the database and adding vehicles functionality within its own class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using CsvHelper;
 using System.Globalization;
 
+
 namespace VehicleManager
 {
     class Program
@@ -10,40 +11,14 @@ namespace VehicleManager
         {
             Console.WriteLine("Initializing Maintenance Program .... ");
             Console.WriteLine("Hello, lets get started entering a vehicle.");
-            Console.WriteLine("Please enter a the Vehicle information:");
-            Console.WriteLine("Make:");
-            var make = Console.ReadLine();
-            Console.WriteLine("Model:");
-            var model = Console.ReadLine();
-            Console.WriteLine("Year:");
-            int year = int.Parse(Console.ReadLine());
-            Console.WriteLine("Vin:");
-            var vin = Console.ReadLine();
-            Console.WriteLine("Current Mileage:");
-            int mileage = int.Parse(Console.ReadLine());
+            var vehicle1 = new Vehicle();
+            vehicle1.ParkVehicle(vehicle1);
 
-            Console.WriteLine($"Awesome! It looks like you have a {year.ToString()} {make} {model} with a whopping {mileage.ToString()} miles! Is that Correct?");
-            Console.ReadKey();
-            Console.WriteLine("What would you like to call this vehicle?");
-            var nickname = Console.ReadLine();
-            Console.WriteLine("Creating a Vehicle in the database...");
-
-            var vehicle1 = new Vehicle(nickname, make, model, year, vin, mileage);
-
-            Console.WriteLine("Looks like your new vehile has been entered into our database. We have a:");
+            Console.WriteLine("Looks like your new vehicle has been entered into our database. We have a:");
             Console.WriteLine($"Make: {vehicle1.Make} \nModel: {vehicle1.Model} \nYear: {vehicle1.Year} \nMileage: {vehicle1.Mileage}");
             Console.ReadKey();
 
-            var vics = new List<Vehicle>
-            {
-                new Vehicle(nickname, make, model, year, vin, mileage),
-            };
 
-            using (var writer = new StreamWriter("/home/osjimene/automotive/Vehicles.csv"))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            {
-                csv.WriteRecords(vics);
-            }
 
 
         }
--- Vehicle.cs
+++ Vehicle.cs
@@ -1,30 +1,68 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using CsvHelper;
+using System.Globalization;
 
 namespace VehicleManager
 {
     public class Vehicle
     {
-        public string Nickname { get; set;}
-        public string Make {get;}
-        public string Model {get;}
+        public string? Nickname { get; set;}
+        public string? Make {get;}
+        public string? Model {get;}
         public int Year {get;}
-        public string Vin {get;}
+        public string? Vin {get;}
         public int Mileage {get; set;}
 
         //Create a list that tracks all the faults on a vehicle.
         public List<Fault> allFaults = new List<Fault>();
+        //Create a list that tracks all the vehicles in the Database.
+        public List<Vehicle> vics = new List<Vehicle>();
+        
+        
 
-        public Vehicle(string nickname, string make, string model, int year, string vin, int mileage)
+        //public Vehicle(string nickname, string make, string model, int year, string vin, int mileage)
+        public Vehicle()
         {
+            Console.WriteLine("Please enter a the Vehicle information:");
+            Console.WriteLine("Make:");
+            var make = Console.ReadLine();
+            Console.WriteLine("Model:");
+            var model = Console.ReadLine();
+            Console.WriteLine("Year:");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vin:");
+            var vin = Console.ReadLine();
+            Console.WriteLine("Current Mileage:");
+            int mileage = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Awesome! It looks like you have a {year.ToString()} {make} {model} with a whopping {mileage.ToString()} miles! Is that Correct?");
+            Console.ReadKey();
+            Console.WriteLine("What would you like to call this vehicle?");
+            var nickname = Console.ReadLine();
+            Console.WriteLine("Creating your vehicle...");
+            
+
             Nickname = nickname;
             Make = make;
             Model = model;
             Year = year;
             Vin = vin;
             Mileage = mileage;
+           
+        }
+
+        public void ParkVehicle(Vehicle obj)
+        {
+            using (var writer = new StreamWriter("/home/osjimene/automotive/Vehicles.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {                               
+                vics.Add(obj);
+                csv.WriteRecords(vics);
+            } 
         }
         
+
     }
 }
